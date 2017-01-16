3784122f0be910fa94e3e65fbf8b6f865bd63c5b
marineco <marinecolonge@gmail.com>
2017-01-16 23:42:18 +0100

    affichage des données quand les champs sont remplis et lorsqu'ils sont vides affichage du formulaire

--- exercice5.php
+++ exercice5.php
@@ -1,27 +1,67 @@
-<!DOCTYPE html>
+<?php
+
+	class Infos {
+
+		private $civilite = "";
+		private $firstname = "";
+		private $lastname = "";
+
+		public function __construct($str1, $str2, $str3) {
+			$this->civilite = $str1;
+			$this->firstname = $str2;
+			$this->lastname = $str3;
+		}
+
+		public function setCivilite() {
+			$this->civilite = $_GET['civilite'];
+			return $this->civilite;
+		}
+
+		public function setFirstname() {
+			$this->firstname = $_GET['firstname'];
+			return $this->firstname;
+		}
+
+		public function setLastname() {
+			$this->lastname = $_GET['lastname'];
+			return $this->lastname;
+		}
+	}
+
+	$getInfos = new Infos();
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Exercice 5 : form avec POO</title>
+	<title>Exercice 5 et 6 : form avec POO</title>
 </head>
 <body>
 	
-	<form method="POST" action="exercice5.php">
-		
-		<select name="civilite" id="civilite">
-			<option value="Mme">Mme</option>
-			<option value="Mr">Mr</option>
-		</select>
+	<?php if (empty($_GET['firstname']) && empty($_GET['lastname'])) : ?>
 
-		<label for="lastname">Nom</label>
-		<input id="lastname" name="lastname" type="text"/>
+		<form method="GET" action="exercice5.php">
+			
+			<select name="civilite" id="civilite">
+				<option value="Mme">Mme</option>
+				<option value="Mr">Mr</option>
+			</select>
 
-		<label for="firstname">Prénom</label>
-		<input id="firstname" name="firstname" type="text"/>
+			<label for="lastname">Nom</label>
+			<input id="lastname" name="lastname" type="text"/>
 
-		<input type="submit" value="Valider"/>
+			<label for="firstname">Prénom</label>
+			<input id="firstname" name="firstname" type="text"/>
 
-	</form>
+			<input type="submit" value="Valider"/>
+
+		</form>
+
+	<?php else : ?>
+
+		<div><?= $getInfos->setCivilite()." ".$getInfos->setFirstname()." ".$getInfos->setLastname(); ?></div>
+
+	<?php endif; ?>
 
 </body>
 </html>
