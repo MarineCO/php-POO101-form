80413e07aebc196f2bee7d1c5f2563856f6a0257
marineco <marinecolonge@gmail.com>
2017-01-16 16:07:00 +0100

    exercice 2 check : création de formulaire demandant le nom et le prénom avec methode post

---
+++ exercice2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exerice 2 : form avec POO</title>
+</head>
+<body>
+
+	<form method="POST" action="exercice2.php">
+		
+		<label for="lastname">Nom</label>
+		<input id="lastname" name="lastname" type="text"/>
+
+		<label for="firstname">Prénom</label>
+		<input id ="firstname" name="firstname" type="text"/>
+
+		<input type="submit" value="Valider"/>
+
+	</form>
+	
+</body>
+</html>
