b8457212c5d709b43168e965fe07804a44f53987
marineco <marinecolonge@gmail.com>
2017-01-16 16:04:22 +0100

    exercice 1 check : création formulaire demandant le nom et le prénom

---
+++ exercice1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exerice 1 : form avec POO</title>
+</head>
+<body>
+
+	<form method="GET" action="exercice1;php">
+		
+		<label for="lastname">Nom</label>
+		<input id="lastname" name="lastname" type="text"/>
+
+		<label for="firstname">Prénom</label>
+		<input id ="firstname" name="firstname" type="text"/>
+
+		<input type="submit" value="Valider"/>
+
+	</form>
+	
+</body>
+</html>
