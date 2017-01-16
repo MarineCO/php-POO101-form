f13779bd5a02df016b8491c77a064951b22acc28
marineco <marinecolonge@gmail.com>
2017-01-16 23:01:05 +0100

    affichage des données du formulaire de l'exercice 1, exercice 4 check

--- user.php
+++ user.php
@@ -1,6 +1,7 @@
 <?php
 
-	class User {
+
+	class UserForm {
 
 		private $firstname = "";
 		private $lastname= "";
@@ -19,11 +20,40 @@
 			$this->lastname = $_GET['lastname'];
 			return $this->lastname;
 		}
-
 	}
 
-	$myUser = new User();
+	$myUser = new UserForm();
+
+	echo '<h1>Exercice 3</h1>';
 	echo '<div>' .$myUser->setLastname(). '</div>';
 	echo '<div>' .$myUser->setFirstname(). '</div>';
+
+
+	class OtherUserForm {
+
+		private $firstname = "";
+		private $lastname = "";
+
+		public function __construct($prenom1, $nom1) {
+			$this->firstname = $prenom1;
+			$this->lastname = $nom1;
+		}
+
+		public function setFirstnameBis() {
+			$this->firstname = $_POST['firstname'];
+			return $this->firstname;
+		}
+
+		public function setLastnameBis() {
+			$this->lastname = $_POST['lastname'];
+			return $this->lastname;
+		}
+	}
+
+	$myOtherUser = new OtherUserForm();
+
+	echo '<h1>Exercice 4</h1>';
+	echo '<div>' .$myOtherUser->setLastnameBis(). '</div>';
+	echo '<div>' .$myOtherUser->setFirstnameBis(). '</div>';
 
 ?>
